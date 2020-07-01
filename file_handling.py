99f9b701f228f9fb28a5add323af124c52e4c715
Sedreh <43466727+Sedreh@users.noreply.github.com>
2020-07-01 12:56:38 +0300

    Update file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -15,3 +15,22 @@ oh.close()
 
 #-----------------------------------
 
+#################################### 
+# reading 16s_extracted file as dataframes
+extracted_df = pd.read_csv("/home/is6/ASVs_comparison/modify1.csv", sep=',', header=None)
+extracted_df.head()
+
+extracted_df.columns = ['sseqid', 'None', 'species']
+extracted_df = extracted_df.drop(extracted_df.index[0])
+
+
+extracted_df = extracted_df.drop('None', 1)
+extracted_df.head()
+
+
+# merge final dataframe on 'sseqid' to 16s_extracted dataframe to get species name
+final_data = pd.merge(extracted_df, N_data, on="sseqid")
+final_data.head()
+
+final_data.to_csv ("/home/is6/ASVs_comparison/16s_fastas/BLAST_results.csv", index = False)
+#-----------------------------------
